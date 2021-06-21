86e2d2d1239e85bb77027ef6697bdb4e40d70ee0
Salim <vsalim1998@gmail.com>
2021-06-21 23:35:20 +0300

    commit 2

--- app/src/main/java/com/rsschool/quiz/ShareResultActivity.kt
+++ app/src/main/java/com/rsschool/quiz/ShareResultActivity.kt
@@ -28,43 +28,15 @@ class ShareResultActivity : AppCompatActivity() {
             val myIntent = Intent()
             myIntent.setAction(Intent.ACTION_SEND)
             myIntent.putExtra(Intent.EXTRA_TEXT, "Your result: $sum%\n" +
-                    "\nВопрос 1: ${questionAnswer[0].question} \n Варианты ответов: \n1. ${questionAnswer[0].arrayAnswers[0]} \n" +
-                    "2. ${questionAnswer[0].arrayAnswers[1]} \n" +
-                    "3. ${questionAnswer[0].arrayAnswers[2]} \n" +
-                    "4. ${questionAnswer[0].arrayAnswers[3]} \n" +
-                    "5. ${questionAnswer[0].arrayAnswers[4]} \n" +
+                    "\nВопрос 1: ${questionAnswer[0].question} \n Варианты ответов: \n" +
                     "Ваш ответ - ${questionAnswer[0].arrayAnswers[q1]}" +
                     "\nВопрос 2: ${questionAnswer[1].question} \n" +
-                    " Варианты ответов: \n" +
-                    "1. ${questionAnswer[1].arrayAnswers[0]} \n" +
-                    "2. ${questionAnswer[1].arrayAnswers[1]} \n" +
-                    "3. ${questionAnswer[1].arrayAnswers[2]} \n" +
-                    "4. ${questionAnswer[1].arrayAnswers[3]} \n" +
-                    "5. ${questionAnswer[1].arrayAnswers[4]} \n" +
                     "Ваш ответ - ${questionAnswer[1].arrayAnswers[q2]}" +
                     "\nВопрос 3: ${questionAnswer[2].question} \n" +
-                    " Варианты ответов: \n" +
-                    "1. ${questionAnswer[2].arrayAnswers[0]} \n" +
-                    "2. ${questionAnswer[2].arrayAnswers[1]} \n" +
-                    "3. ${questionAnswer[2].arrayAnswers[2]} \n" +
-                    "4. ${questionAnswer[2].arrayAnswers[3]} \n" +
-                    "5. ${questionAnswer[2].arrayAnswers[4]} \n" +
                     "Ваш ответ - ${questionAnswer[2].arrayAnswers[q3]}" +
                     "\nВопрос 4: ${questionAnswer[3].question} \n" +
-                    " Варианты ответов: \n" +
-                    "1. ${questionAnswer[3].arrayAnswers[0]} \n" +
-                    "2. ${questionAnswer[3].arrayAnswers[1]} \n" +
-                    "3. ${questionAnswer[3].arrayAnswers[2]} \n" +
-                    "4. ${questionAnswer[3].arrayAnswers[3]} \n" +
-                    "5. ${questionAnswer[3].arrayAnswers[4]} \n" +
                     "Ваш ответ - ${questionAnswer[3].arrayAnswers[q4]}" +
                     "\nВопрос 5: ${questionAnswer[4].question} \n" +
-                    " Варианты ответов: \n" +
-                    "1. ${questionAnswer[4].arrayAnswers[0]} \n" +
-                    "2. ${questionAnswer[4].arrayAnswers[1]} \n" +
-                    "3. ${questionAnswer[4].arrayAnswers[2]} \n" +
-                    "4. ${questionAnswer[4].arrayAnswers[3]} \n" +
-                    "5. ${questionAnswer[4].arrayAnswers[4]} \n" +
                     "Ваш ответ - ${questionAnswer[4].arrayAnswers[q5]}"
             )
             myIntent.setType("text/plain")
